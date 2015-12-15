b0c7329b7ac7c046796078c3393512a8e613e796
Catie Jo <catiejo.pidel@gmail.com>
2015-12-15 12:26:46 -0600

    centered image and coverable button

--- meat.js
+++ meat.js
@@ -1,7 +1,35 @@
-var main = function() {
-  $('.pic').click(function() {
-    confirm("This is TACOSAUR.");
+var main = function () {
+  $('.button').mouseenter(function() {
+      $('.button').fadeTo('fast', 1);
   });
+
+  $('.button').mouseleave(function() {
+      $('.button').fadeTo('fast', 0.5);
+  });
+
+/*
+  $('.button').click(function() {
+    $('.button').toggle(red, orange, yellow);
+  });
+*/
+
+/*
+  $('.tacotaco').click(function() {
+    $('.tacotaco').confirm("Click 'ok' to accept me as master of the INTERNET");
+  });
+*/
+}
+
+var red = function() {
+  $('.button').css('background-color', 'red');
+}
+
+var orange = function() {
+  $('.button').css('background-color', 'orange');
+}
+
+var yellow = function() {
+  $('.button').css('background-color', 'yellow');
 }
 
 $(document).ready(main);
